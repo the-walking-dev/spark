ac6f002aff53383cf73e0d561a9b9738c18fa9ad
Jose Moreno <jomoespe@gmail.com>
2018-04-20 14:55:38 +0200

    Add WordCount scala script

---
+++ src/scala/WordCount.scala
@@ -0,0 +1,8 @@
+val textFile   = sc.textFile("README.md")
+val wordCounts = { textFile
+                    .flatMap(line => line.split(" "))
+                    .map(word => (word, 1))
+                    .reduceByKey((a, b) => a + b)
+                 }
+wordCounts.collect();
+System.exit(0);
